411e6fc93e8aa07652caa1b6d5345d04146290e2
pedroaz <pedro.azvm@gmail.com>
2022-05-27 10:14:40 -0300

    Adding envrionment

--- functions/hello.py
+++ functions/hello.py
@@ -1,17 +1,9 @@
 
+import requests
+import os
 
 def hello_http(request):
-
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello2 {}!'.format(escape(name))
-
-def hello_http2(request):
-    return "Hello Again"
+    print("Calling API")
+    print("Env: " + os.environ["bye_url"])
+    r = requests.get(os.environ["bye_url"])
+    return "Hello " + r.text
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 
-import importlib
 from flask import escape
 import functions_framework
 
 from functions.hello import *
 from functions.bye import *
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 @functions_framework.http
 def hello(request):
-    return hello_http2(request)
+    return hello_http(request)
 
 def bye(request):
     return bye_http(request)
